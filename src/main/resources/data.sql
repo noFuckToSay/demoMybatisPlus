c40f667c49bcd9178c0464d5a2ae075a8b537f59
丁志康 <dingzhikang@haier.com>
2024-04-30 17:59:01 +0800

    hdsvmi-813 feat 【料废】过程效率运营管理报表后端功能开发28

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,7 @@
-select 'begin DDL...';
+INSERT INTO `user`(`id`,`user_name`,`password`,`create_by`,`update_by`)
+    VALUES(FLOOR(RAND() * (1000000000000000 -  + 1)),'admin','admin','admin','admin') AS init_user
+ON DUPLICATE KEY UPDATE
+                     `user_name`=init_user.`user_name`,
+                     `password`=init_user.`password`,
+                     `create_by`=init_user.`create_by`,
+                     `update_by`=init_user.`update_by`;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,13 @@
-select 'begin DML...';
+CREATE TABLE IF NOT EXISTS `user`
+(
+    `id`          BIGINT      NOT NULL COMMENT '主键',
+    `user_name`   VARCHAR(32) NOT NULL COMMENT '用户名',
+    `password`    VARCHAR(32) NOT NULL COMMENT '密码',
+    `create_time` DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_time` DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+    `create_by`   VARCHAR(32)          DEFAULT NULL COMMENT '创建人',
+    `update_by`   VARCHAR(32)          DEFAULT NULL COMMENT '修改人',
+    `is_del`      INT         NOT NULL DEFAULT '0' COMMENT '逻辑删除',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `user_name_unique` (`user_name`)
+) ENGINE = INNODB COMMENT ='用户表';
