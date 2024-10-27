88225b6ef08ab735160160a1bdb796866f85c98b
丁志康 <dingzhikang@haier.com>
2024-10-28 00:39:24 +0800

    mapper扫描包名大小写错误

--- src/main/java/com/example/demoMybatisPlus/DemoMybatisPlusApplication.java
+++ src/main/java/com/example/demoMybatisPlus/DemoMybatisPlusApplication.java
@@ -3,17 +3,24 @@ package com.example.demoMybatisPlus;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@MapperScan({"com.example.demomybatisplus.*.mapper"})
+@MapperScan({"com.example.demoMybatisPlus.*.mapper"})
 @EnableFeignClients
 @EnableAsync
-public class DemoMybatisPlusApplication {
+public class DemoMybatisPlusApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoMybatisPlusApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoMybatisPlusApplication.class);
     }
 
 }
